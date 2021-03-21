4cf2dc00359411e2debe14b7a287e924b533dae7
Edoardo Ranghieri <theedoran@gmail.com>
2021-03-21 01:33:15 +0100

    Memoization for just an hour

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ import memoize from "memoizee";
 const searchByName = (query) =>
   axios.get(`https://www.speedtest.net/api/js/servers?search=${query}`);
 
-// Memoize for 12 hours.
-const memoOpts = { promise: true, maxAge: 43200 * 1000 };
+// Memoize for 1 hour.
+const memoOpts = { promise: true, maxAge: 3600 * 1000 };
 
 export const memoSearchByName = memoize(searchByName, memoOpts);
