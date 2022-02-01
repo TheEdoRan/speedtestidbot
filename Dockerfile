f1395c210d73f4db7ed8141e50a8c1dbc6f24ce0
Edoardo Ranghieri <edoardo@ranghieri.com>
2022-02-01 12:34:10 +0100

    style(lint): use ESLint, lint-staged, Husky, commitlint

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ COPY package*.json ./
 RUN npm ci
 COPY tsconfig.json ./
 COPY src src
-RUN npm run build
+RUN npm run compile
 
 # Run
 FROM node:16-alpine AS runner
@@ -14,7 +14,7 @@ WORKDIR /usr/src/app
 RUN chown node:node .
 USER node
 COPY --chown=node:node package*.json ./
-RUN npm ci
+RUN npm ci --ignore-scripts
 COPY --chown=node:node --from=builder /usr/src/app/dist/ .
 EXPOSE 8080
 CMD ["node", "bot.js"]
--- src/api/index.ts
+++ src/api/index.ts
@@ -1,14 +1,15 @@
 import axios, { AxiosResponse } from "axios";
+
 import { SpeedtestServer } from "./types";
 
 // Search server via its name.
 // Limit the search to the first 10 matching results.
 export const searchByName = async (
-  query: string
+	query: string
 ): Promise<AxiosResponse<SpeedtestServer[]>> =>
-  axios.get(
-    `https://www.speedtest.net/api/js/servers?search=${encodeURIComponent(
-      query
-    )}&limit=10`,
-    { timeout: 2000 }
-  );
+	axios.get(
+		`https://www.speedtest.net/api/js/servers?search=${encodeURIComponent(
+			query
+		)}&limit=10`,
+		{ timeout: 2000 }
+	);
