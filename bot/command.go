724f55f5e91b7911b2bdeb47574ee00aa06b6d31
Edoardo Ranghieri <edoardo@ranghieri.com>
2022-02-19 13:11:26 +0100

    style: use const for parse mode

--- bot/command.go
+++ bot/command.go
@@ -9,9 +9,10 @@ import (
 // HandleStartCommand handles /start in chats.
 func HandleStartCommand(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
 	firstName := message.From.FirstName
-	txt := fmt.Sprintf(StartCommandReply, tgbotapi.EscapeText("HTML", firstName))
+	txt := fmt.Sprintf(StartCommandReply, tgbotapi.EscapeText(tgbotapi.ModeHTML, firstName))
 	m := tgbotapi.NewMessage(message.Chat.ID, txt)
-	m.ParseMode = "HTML"
+	m.ParseMode = tgbotapi.ModeHTML
+
 	m.ReplyToMessageID = message.MessageID
 
 	if _, err := bot.Send(m); err != nil {
